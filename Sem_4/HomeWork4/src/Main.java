9965e08b71821f607df06f4ee1b1110723ba6c8d
Dmitriy <dima4o81@gmail.com>
2022-12-03 22:17:49 +0300

    Complete HomeWork4, upgrade FamilyTree

---
+++ Sem_4/HomeWork4/src/Main.java
@@ -0,0 +1,8 @@
+public class Main { // Класс, содержащий основную функцию
+
+    public static void main(String[] args) { // Параметр args указывает аргументы командной строки
+
+        new TreeGUI();
+    }
+}
+
