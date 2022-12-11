9ba2d2f8e77975431d0e18d402885bc43df62fcc
Dmitriy <dima4o81@gmail.com>
2022-12-11 20:12:22 +0300

    Complete HomeWork Seminar 6

---
+++ Sem_6/MyCalc/src/Calculator/MyDrawPad.java
@@ -0,0 +1,22 @@
+package Calculator;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+
+public class MyDrawPad implements MouseMotionListener, ActionListener {
+    @Override
+    public void mouseDragged(MouseEvent e){
+
+    }
+    @Override
+    public void mouseMoved(MouseEvent e){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
